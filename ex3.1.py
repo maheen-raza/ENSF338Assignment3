16fd568f3140f99899b526fbb9e217cf2e78bb22
maheen-raza <113572260+maheen-raza@users.noreply.github.com>
2023-02-23 16:32:15 -0700

    ex3.3.py done

--- ex3.1.py
+++ ex3.1.py
@@ -1,1 +1,64 @@
+class Stack:
+    def __init__(self):
+        self.items = []
+        
+    def push(self, item):
+        self.items.append(item)
+        
+    def pop(self):
+        return self.items.pop()
+    
+    def is_empty(self):
+        return self.items == []
+    
+    def peek(self):
+        if not self.is_empty():
+            return self.items[-1]
+    
+def tokenize(expr):
+    for p in ['(',')','[',']','{','}']:
+        expr = expr.replace(p, f' {p} ')
+    return expr.split()
+
+def evaluate(expr):
+    tokens = tokenize(expr)
+    
+    stack = Stack()
+    
+    for token in tokens:
+        if token == '(':
+            pass
+        elif token in ['+', '-', '*', '/']:
+            op = token
+        elif token.isdigit():
+            e2 = int(token)
+            e1 = int(token)
+        
+            if op == '+':
+                result = e1 + e2
+            elif op == '-':
+                result = e1 - e2
+            elif op == '*':
+                result = e1 * e2
+            elif op == '/':
+                result = e1 / e2
+            stack.push(result)
+        elif token == ')':
+            pass
+        else:
+            stack.push(int(token))
+    
+    return stack.pop()
+
+input = input()
+output = evaluate(input)
+print(output)
+
+#print(evaluate("(+ 1 2)"))
+
+#print(evaluate("(* 3 (- 4 2))"))
+
+#print(evaluate("(/ 10 (+ 2 3))"))
+
+
 
--- ex3.3.py
+++ ex3.3.py
@@ -1,1 +1,15 @@
+import sys
 
+my_list = []
+
+size = sys.getsizeof(my_list)
+
+for i in range(64):
+    my_list.append(i)
+    new_size = sys.getsizeof(my_list)
+    if new_size != size:
+        print(f"Capacity changed from {size} byte to {new_size} bytes.")
+        size = new_size
+
+
+print(my_list)
