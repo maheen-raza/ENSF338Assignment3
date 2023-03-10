14b8a10bb16db0814d05b30689782918a05444e7
anitasjohn <113326608+anitasjohn@users.noreply.github.com>
2023-03-10 00:24:22 -0600

    done

--- ex3.2.py
+++ ex3.2.py
@@ -28,22 +28,22 @@ min_midpoint = max(len(array) // 100, 1)
 midrange = range(min_midpoint, len(array), len(array) // 100)
 
 best_midpoints = {}
-for each_number in search_tasks:
-    if each_number not in array:
-        continue
-    goat_midrange = 0
-    best_time = float('inf')
+for i in range(100):  # iterate 100 times
+    for each_number in search_tasks:
+        if each_number not in array:
+            continue
+        goat_midrange = 0
+        best_time = float('inf')
 
-    for midpoint in midrange:   
-        start_time = time.perf_counter()
-        match = binarysearch(array, each_number, midpoint)
-        end_time = time.perf_counter()
-        difference_time = end_time - start_time
-        if match and difference_time < best_time:
-            best_time = difference_time
-            goat_midrange = midpoint
-    best_midpoints[each_number] = goat_midrange
-    print(f"For {each_number}, best midpoint is {goat_midrange:.10f} with time: {best_time:.10f} seconds")
+        for midpoint in midrange:
+            start_time = time.perf_counter()
+            match = binarysearch(array, each_number, midpoint)
+            end_time = time.perf_counter()
+            difference_time = end_time - start_time
+            if match and difference_time < best_time:
+                best_time = difference_time
+                goat_midrange = midpoint
+        best_midpoints[each_number] = goat_midrange
 
 x,y=zip(*best_midpoints.items())
 
