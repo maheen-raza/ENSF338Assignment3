94499ebd34ad0d7b7ba43bb72745cfcd67f7ff53
anitasjohn <113326608+anitasjohn@users.noreply.github.com>
2023-03-09 20:05:47 -0600

    ex3, 1 and 2 done

--- ex3.2.py
+++ ex3.2.py
@@ -1,1 +1,37 @@
+import json
 
+
+def binarysearch(array, checknumber, startmid):
+    left=0
+    right= len(array)-1
+    mid = startmid
+
+    while left <= right:
+        if array[mid] == checknumber:
+            return True
+
+        elif array[mid] < checknumber:
+            left = mid +1
+        else:
+            right = mid -1
+        mid = (left+right)//2
+
+
+
+
+
+
+
+
+with open('ex2data.json') as f:
+    array = json.load(f)
+
+with open('ex2tasks.json') as f:
+    searchtasks = json.load(f)
+
+for each_number in searchtasks:
+    match = binarysearch(array, each_number, (len(array)//2))
+
+    if match:
+        print(f"{each_number} is in the array")
+    
