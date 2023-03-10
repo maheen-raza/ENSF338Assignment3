67a399863e77c576ee2c153f705a7073c201a995
anitasjohn <113326608+anitasjohn@users.noreply.github.com>
2023-03-09 21:29:33 -0600

    this should work

--- ex3.2.py
+++ ex3.2.py
@@ -35,6 +35,7 @@ with open('ex2tasks.json') as f:
 
 midrange = range(len(array)// 100, len(array), len(array)// 100)
 
+# create lists to store task and midpoint data
 task_list = []
 midpoint_list = []
 
@@ -50,9 +51,20 @@ for each_number in searchtasks:
         if match and differencetime < besttime:
             besttime = differencetime
             goatmidrange = midpoint
-        #print(f"For {each_number}, best midpoint is {goatmidrange} with time: {besttime:.10f} seconds")
+
+    # add task and midpoint to lists
+    task_list.append(each_number)
+    midpoint_list.append(goatmidrange)
+
+# create scatter plot of task vs. midpoint
 plt.scatter(task_list, midpoint_list)
 plt.xlabel('Task')
 plt.ylabel('Midpoint')
 plt.title('Chosen Midpoint for Each Task')
+
+# create line plot of all midpoints
+x = np.array(list(range(len(searchtasks))))
+y = np.array([midrange[i // 100] for i in range(len(searchtasks))])
+plt.plot(x, y, color='red')
+
 plt.show()
