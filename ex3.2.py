4567c52874c865295ad1cf2ac61a7842c45547d7
anitasjohn <113326608+anitasjohn@users.noreply.github.com>
2023-03-09 22:13:07 -0600

    NEW

--- ex3.2.py
+++ ex3.2.py
@@ -23,14 +23,18 @@ with open('ex2data.json') as f:
 with open('ex2tasks.json') as f:
     search_tasks = json.load(f)
 
-midrange = range(len(array) // 50, len(array), len(array) // 50)
+# define the range of midpoints to try
+min_midpoint = max(len(array) // 100, 1)
+midrange = range(min_midpoint, len(array), len(array) // 100)
 
 best_midpoints = {}
 for each_number in search_tasks:
+    if each_number not in array:
+        continue
     goat_midrange = 0
     best_time = float('inf')
 
-    for midpoint in midrange:
+    for midpoint in midrange:   
         start_time = time.perf_counter()
         match = binarysearch(array, each_number, midpoint)
         end_time = time.perf_counter()
@@ -39,10 +43,9 @@ for each_number in search_tasks:
             best_time = difference_time
             goat_midrange = midpoint
     best_midpoints[each_number] = goat_midrange
-    print(f"For {each_number}, best midpoint is {goat_midrange} with time: {best_time:.10f} seconds")
+    print(f"For {each_number}, best midpoint is {goat_midrange:.10f} with time: {best_time:.10f} seconds")
 
-x = list(best_midpoints.keys())
-y = list(best_midpoints.values())
+x,y=zip(*best_midpoints.items())
 
 plt.scatter(x, y)
 plt.title('Best Midpoints for Binary Search')
