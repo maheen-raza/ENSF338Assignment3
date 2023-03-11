e11deb4366c4e2331a8f965658c73597faeb3018
anitasjohn <113326608+anitasjohn@users.noreply.github.com>
2023-03-11 15:55:42 -0600

    ex3.4

--- ex3.4.py
+++ ex3.4.py
@@ -1,4 +1,3 @@
-
 import threading
 import random
 import time
@@ -17,22 +16,56 @@ class CircularQueue:
         self._lock.release()
 
     def enqueue(self, data):
-        # Implement enqueue function
-        pass
-    
+        while True:
+            self.lock()
+            if (self.tail + 1) % self.size == self.head:
+                # Queue is full, wait one second and try again
+                print('Queue is full, waiting...')
+                self.unlock()
+                time.sleep(1)
+            else:
+                if self.head == -1:
+                    # First element being added
+                    self.head = 0
+                self.tail = (self.tail + 1) % self.size
+                self.queue[self.tail] = data
+                print(f'Enqueued {data} at index {self.tail}')
+                self.unlock()
+                return
+
     def dequeue(self):
-        # Implement dequeue function
-        pass
+        while True:
+            self.lock()
+            if self.head == -1:
+                # Queue is empty, wait one second and try again
+                print('Queue is empty, waiting...')
+                self.unlock()
+                time.sleep(1)
+            else:
+                data = self.queue[self.head]
+                if self.head == self.tail:
+                    # Last element being removed
+                    self.head = self.tail = -1
+                else:
+                    self.head = (self.head + 1) % self.size
+                print(f'Dequeued {data} from index {self.head}')
+                self.unlock()
+                return data
 
 def producer():
     while True:
-        # Implement producer function
-        pass
+        data = random.randint(1, 10)
+        print(f'Producer generated {data}, waiting {data} seconds...')
+        time.sleep(data)
+        q.enqueue(data)
 
 def consumer():
     while True:
-        # Implement consumer function
-        pass
+        data = random.randint(1, 10)
+        print(f'Consumer waiting {data} seconds...')
+        time.sleep(data)
+        item = q.dequeue()
+        print(f'Consumer dequeued {item}')
 
 if __name__ == '__main__':
     q = CircularQueue(5)
