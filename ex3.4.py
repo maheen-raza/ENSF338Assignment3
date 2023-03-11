ff88c7b9f38866512db78aa61f65db5f2437882e
anitasjohn <113326608+anitasjohn@users.noreply.github.com>
2023-03-11 15:19:43 -0600

    skeleton  code

--- ex3.4.py
+++ ex3.4.py
@@ -1,1 +1,44 @@
 
+import threading
+import random
+import time
+
+class CircularQueue:
+    def __init__(self, size):
+        self.size = size
+        self.queue = [None for i in range(size)]
+        self.head = self.tail = -1
+        self._lock = threading.Lock()
+
+    def lock(self):
+        self._lock.acquire()
+
+    def unlock(self):
+        self._lock.release()
+
+    def enqueue(self, data):
+        # Implement enqueue function
+        pass
+    
+    def dequeue(self):
+        # Implement dequeue function
+        pass
+
+def producer():
+    while True:
+        # Implement producer function
+        pass
+
+def consumer():
+    while True:
+        # Implement consumer function
+        pass
+
+if __name__ == '__main__':
+    q = CircularQueue(5)
+    t1 = threading.Thread(target=producer)
+    t2 = threading.Thread(target=consumer)
+    t1.start()
+    t2.start()
+    t1.join()
+    t2.join()
